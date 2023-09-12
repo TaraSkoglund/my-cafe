79682fe0fcb5e452be28a5c8e96fa24e3b923264
Tara Skoglund <skoglundtara@gmail.com>
2023-09-12 14:26:41 +0200

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center p-6">
+      <div>My-Cafe</div>
+      <nav>
+        <ul className="flex space-x-6">
+          <li className="hover:bg-slate-200 p-1">Vårt Sortiment</li>
+          <li className="hover:bg-slate-200 p-1">Om Oss</li>
+          <li className="hover:bg-slate-200 p-1">Kontakta Oss</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
