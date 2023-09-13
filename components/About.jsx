0f046a868f3f9a1a6e576499f18fb7b0703f618f
Tara Skoglund <skoglundtara@gmail.com>
2023-09-13 15:30:09 +0200

    Added component About

---
+++ components/About.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <section className="m-12 font-serif">
+      <h1 className="text-2xl py-6">Om Oss</h1>
+      <p className="px-4">
+        My Cafe är ett familjeägt hantverksbageri och konditori som funnits i
+        Göteborgsområdet sedan starten 1962. Allt startade i familjen Olssons
+        källare i Mölndal och har nu expanderat till fem butiker runt om i
+        Göteborgsområdet. Vår största butik ligger på Västra Hamngatan i
+        centrala Göteborg. På My Cafe värdesätts ett noggrant hantverk från
+        grunden och kvalitativa råvaror.
+        <br />
+        <br /> Vårt bröd bakas med surdeg på lokalt mjöl av högsta kvalitet från
+        Berte Qvarn. För att få den härligt krispiga ytan med det mjuka inkråmet
+        bakas det i vår stenugn i Mölndal. Surdegen är en central del av My Cafe
+        och alla bröd bakas på det gamla goda viset, det är då det blir som
+        bäst. Vi är även välkända för vårt stora utbud av fantastiska tårtor,
+        och vår prinsesstårta har blivit utsedd till Göteborgs godaste
+        prinsesstårta i GP 2022. Det går även att få unika fototryck på våra
+        bildtårtor och lägga till rosor eller text för en mer personlig tårta.
+      </p>
+    </section>
+  );
+}
