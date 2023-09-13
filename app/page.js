8661fc998fa29ef9f998379122881ca8f6b4f187
Tara Skoglund <skoglundtara@gmail.com>
2023-09-13 11:09:06 +0200

    Added filtering buttons

--- app/page.js
+++ app/page.js
@@ -2,8 +2,19 @@ export default function Home() {
   return (
     <main>
       <div>
-        <section className="m-12 ">
-          <h1 className="font-serif text-2xl">Sortiment</h1>
+        <section className="m-12 font-serif">
+          <h1 className="text-2xl">Sortiment</h1>
+          <div className="flex space-x-6 my-12">
+            <button className="py-3 px-9 border rounded-sm border-black">
+              Bullar
+            </button>
+            <button className="py-3 px-9 border rounded-sm border-black">
+              Tårtor
+            </button>
+            <button className="py-3 px-9 border rounded-sm border-black">
+              Kakor
+            </button>
+          </div>
         </section>
       </div>
     </main>
