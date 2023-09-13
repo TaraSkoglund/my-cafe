5671f8579d922bc0d8faa035efa3c60c46c36d04
Tara Skoglund <skoglundtara@gmail.com>
2023-09-13 11:47:51 +0200

    changed breakpoint

--- app/page.js
+++ app/page.js
@@ -5,13 +5,13 @@ export default function Home() {
         <section className="m-12 font-serif">
           <h1 className="text-2xl">Sortiment</h1>
           <div className="flex space-x-3 my-12 md:space-x-6">
-            <button className="py-1 px-6 border rounded-sm border-black sm: py-3 px-9 ">
+            <button className="py-1 px-6 border rounded-sm border-black md: py-3 px-9 ">
               Bullar
             </button>
-            <button className="py-1 px-6 border rounded-sm border-black sm: py-3 px-9 ">
+            <button className="py-1 px-6 border rounded-sm border-black md: py-3 px-9 ">
               Tårtor
             </button>
-            <button className="py-1 px-6 border rounded-sm border-black sm: py-3 px-9 ">
+            <button className="py-1 px-6 border rounded-sm border-black md: py-3 px-9 ">
               Kakor
             </button>
           </div>
