bfcc6aaed8ef0fad5ae87308342a219b9d6cf209
Tara Skoglund <skoglundtara@gmail.com>
2023-09-14 13:25:43 +0200

    added mockdata

---
+++ constants/index.js
@@ -0,0 +1,76 @@
+export const MOCK_PRODUCTS = [
+  {
+    id: 1,
+    typ: "Kaka",
+    title: "Hallongrotor",
+    img: "@/assets/kaka1.jpeg",
+    ingredients: [
+      "Vetemjöl",
+      "smör",
+      "strösocker",
+      "vaniljsocker",
+      "hallonsylt",
+      "bakpulver",
+    ],
+    price: 58,
+    description:
+      "Duka upp till kafferep och dessa godbitar är en absolut favorit.Den lilla söta kakan är spröd och fylld med ljuvlig hallonsylt. Du får en fin kakpåse med 10 stycken av dessa godbitar i.",
+  },
+  {
+    id: 2,
+    typ: "Bulle",
+    title: "Winnebulle",
+    img: "@/assets/bulle1.png",
+    ingredients: [
+      "Vetemjöl",
+      "smör",
+      "ägg",
+      "strösocker",
+      "vanilj",
+      "grädde",
+      "mandel",
+      "jäst",
+      "salt",
+    ],
+    price: 29,
+    description:
+      "Vår Winnebulle är ett mästerverk av brödbakning, bakat med omsorg och de finaste ingredienserna. Med sitt karakteristiska utseende och förföriska doft är det en klassisk favorit",
+  },
+  {
+    id: 3,
+    typ: "Bulle",
+    title: "Croissant",
+    img: "@/assets/bulle2.jpeg",
+    ingredients: [
+      "Vetemjöl",
+      "smör",
+      "strösocker",
+      "mjölk",
+      "ägg",
+      "salt",
+      "jäst",
+    ],
+    price: 25,
+    description:
+      "Denna spröda läckerbit med en krispig yta och ett fluffigt, mjukt innehåll vinner de flestas hjärtan. Till frukost, till kaffepausen eller som en mumsbit på språng är vår croissant ett solklart val för många. Bakad på äkta svenskt smör.",
+  },
+  {
+    id: 4,
+    typ: "Tårta",
+    title: "Drottningtårta",
+    img: "@/assets/tårta3.jpeg",
+    ingredients: [
+      "Socker",
+      "ägg",
+      "mjöl",
+      "grädde",
+      "vaniljkräm",
+      "blåbär",
+      "hallon",
+      "bakpulver",
+    ],
+    price: 186,
+    description:
+      "Söta blåbär och syrliga hallon möts i en härlig drottningkompott som varvas med vispad grädde, saftig sockerkaka och egengjord vaniljkräm. Tårtan är toppad med ett rikt lager hallon och blåbär samt en dekorativ kant av spritsad grädde.",
+  },
+];
