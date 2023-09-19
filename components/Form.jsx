e5bf98bb2ea8cf253e77ed4b9458672d5b301e18
Tara Skoglund <skoglundtara@gmail.com>
2023-09-19 10:28:36 +0200

    added form component

---
+++ components/Form.jsx
@@ -0,0 +1,8 @@
+export default function Form() {
+  return (
+    <section className="m-12 font-serif">
+      <h1 className="text-2xl py-6">Om Oss</h1>
+      <p className="px-4 text-sm"></p>
+    </section>
+  );
+}
