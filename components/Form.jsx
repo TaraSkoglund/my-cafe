41e50c867be31b36540d20afcd612235ea12fc33
Tara Skoglund <skoglundtara@gmail.com>
2023-09-20 10:43:46 +0200

    change layout

--- components/Form.jsx
+++ components/Form.jsx
@@ -18,11 +18,13 @@ export default function Form() {
         .max(20, "Must be 20 characters or less")
         .required("required"),
       email: Yup.string().email("Invalid email adress").required("required"),
+      tel: Yup.string()
+        .max(12, "Must be 12 numbers or less")
+        .min(10, "Must be 10 numbers or more")
+        .required("required"),
       massage: Yup.string()
         .max(150, "Must be 150 characters or less")
-        .required("required"),
-      tel: Yup.string()
-        .max(10, "Must be 10 numbers or less")
+        .min(5, "Must be 5 characters or more")
         .required("required"),
     }),
     onSubmit: (values) => {
@@ -33,77 +35,91 @@ export default function Form() {
   return (
     <section className="m-12 font-serif">
       <h1 className="text-2xl py-6">Kontakta oss</h1>
-      <form action="#" className="px-4" onSubmit={formik.handleSubmit}>
-        <div>
-          <input
-            id="firstName"
-            name="firstName"
-            type="text"
-            placeholder="Förnamn"
-            className="form-input border-black rounded mb-6 mr-6 sm:w-full md:w-5/12"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.firstName}
-          />
-          {formik.touched.firstName && formik.errors.firstName ? (
-            <p className="mb-6">{formik.errors.firstName}</p>
-          ) : null}
-          <input
-            id="lastName"
-            name="lastName"
-            type="text"
-            placeholder="Efternamn"
-            className="form-input border-black rounded mb-6 sm:w-full md:w-5/12 "
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.lastName}
-          />
-          {formik.touched.lastName && formik.errors.lastName ? (
-            <p className="mb-6">{formik.errors.lastName}</p>
-          ) : null}
+      <form
+        action="#"
+        className="px-4 max-w-4xl"
+        onSubmit={formik.handleSubmit}
+      >
+        <div className="flex flex-wrap md:flex-nowrap gap-6 mb-6">
+          <div className="flex flex-col w-full">
+            <input
+              id="firstName"
+              name="firstName"
+              type="text"
+              placeholder="Förnamn"
+              className="form-input border-black rounded md:basis-1/2 basis-full"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.firstName}
+            />
+            {formik.touched.firstName && formik.errors.firstName ? (
+              <p className="mb-6">{formik.errors.firstName}</p>
+            ) : null}
+          </div>
+          <div className="flex flex-col w-full">
+            <input
+              id="lastName"
+              name="lastName"
+              type="text"
+              placeholder="Efternamn"
+              className="form-input border-black rounded md:basis-1/2 basis-full  "
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.lastName}
+            />
+            {formik.touched.lastName && formik.errors.lastName ? (
+              <p className="mb-6">{formik.errors.lastName}</p>
+            ) : null}
+          </div>
+        </div>
+        <div className="flex gap-6 mb-6 flex-wrap md:flex-nowrap">
+          <div className="flex flex-col w-full">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              className="form-input border-black rounded md:basis-1/2 basis-full"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <p className="mb-6">{formik.errors.email}</p>
+            ) : null}
+          </div>
+          <div className="flex flex-col w-full">
+            <input
+              id="tel"
+              name="tel"
+              type="number"
+              placeholder="Nummer"
+              className="form-input border-black rounded md:basis-1/2 basis-full"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.tel}
+            />
+            {formik.touched.tel && formik.errors.tel ? (
+              <p className="mb-6">{formik.errors.tel}</p>
+            ) : null}
+          </div>
         </div>
         <div>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Email"
-            className="form-input border-black rounded mb-6 mr-6 sm:w-full md:w-5/12"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <p className="mb-6">{formik.errors.email}</p>
-          ) : null}
-          <input
-            id="tel"
-            name="tel"
-            type="tel"
-            placeholder="Nummer"
-            className="form-input border-black rounded mb-6 sm:w-full md:w-5/12 "
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.tel}
-          />
-          {formik.touched.tel && formik.errors.tel ? (
-            <p className="mb-6">{formik.errors.tel}</p>
-          ) : null}
-        </div>
-        <div>
-          <input
-            id="massage"
-            name="massage"
-            type="text"
-            placeholder="Meddelande"
-            className="form-input border-black rounded mb-6 w-full"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.massage}
-          />
-          {formik.touched.massage && formik.errors.massage ? (
-            <p className="mb-6">{formik.errors.massage}</p>
-          ) : null}
+          <div className="mb-6">
+            <input
+              id="massage"
+              name="massage"
+              type="text"
+              placeholder="Meddelande"
+              className="form-input border-black rounded w-full"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.massage}
+            />
+            {formik.touched.massage && formik.errors.massage ? (
+              <p className="mb-6">{formik.errors.massage}</p>
+            ) : null}
+          </div>
           <button
             type="submit"
             className="py-1 px-6 border rounded-sm border-black hover:shadow-2xl"
