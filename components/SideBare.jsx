d4e31bc0254b47f177ef428a68b9e205187609d1
Tara Skoglund <skoglundtara@gmail.com>
2023-09-25 16:32:57 +0200

    add layout to sidebare

--- components/SideBare.jsx
+++ components/SideBare.jsx
@@ -1,8 +1,50 @@
+"use client";
+import { MinusCircle, PlusCircle } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
 export default function SideBare() {
+  const [activeLink, setActiveLink] = useState("");
   return (
-    <section className="absolute inset-y-0 right-0 w-80 bg-white mt-16">
-      <div>
-        <h1>HEJ</h1>
+    <section className="absolute inset-y-0 right-0 w-80 bg-white mt-16 font-serif flex flex-col">
+      <h1 className="m-3 gap-3">I Korgen</h1>
+      <div className="flex justify-start gap-2 m-3">
+        <div className="w-36 ">
+          <Image
+            src="/assets/tårta1-bred.jpeg"
+            width={500}
+            height={500}
+            alt="Hero Imge"
+            className="object-cover bg-cover bg-center"
+          />
+        </div>
+        <div className="text-md">
+          <p className="pb-2">Hallongrottor</p>
+          <div className="flex flex-col justify-between">
+            <p className="pb-2">136 kr</p>
+            <div className="flex items-center gap-2">
+              <button>
+                <PlusCircle />
+              </button>
+              <p>1</p>
+              <button>
+                <MinusCircle />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="m-3 flex justify-center flex-col">
+        <div>
+          <p>Totalt: 136 kr</p>
+        </div>
+        <button
+          className={`py-1 px-6 border rounded-sm border-black hover:shadow-2xl hover:border-b-2 ${
+            activeLink === "Beställ" ? "border-b-2" : ""
+          }`}
+          onClick={() => setActiveLink("Beställ")}
+        >
+          Beställ
+        </button>
       </div>
     </section>
   );
