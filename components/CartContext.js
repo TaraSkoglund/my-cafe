db9c5ef490a8940a8a6b942cdd6249972435599e
Tara Skoglund <skoglundtara@gmail.com>
2023-09-27 13:03:50 +0200

    Create the CartProvider component

--- components/CartContext.js
+++ components/CartContext.js
@@ -5,3 +5,26 @@ export const CartContext = createContext();
 export const useCart = () => {
   return useContext(CartContext);
 };
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    const updatedCart = cartItems.filter((item) => item.id !== productId);
+    setCartItems(updatedCart);
+  };
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>
+  );
+};
